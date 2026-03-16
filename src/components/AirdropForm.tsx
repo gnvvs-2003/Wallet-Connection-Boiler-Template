6efed20c1c52ba18266311f9547b02de1959416f
gnvvs-2003 <vchess007@gmail.com>
2026-03-16 19:47:20 +0530

    Wagmi read contracts

--- src/components/AirdropForm.tsx
+++ src/components/AirdropForm.tsx
@@ -2,13 +2,67 @@
 
 import { useState } from "react";
 import { InputForm } from "@/components/InputField";
+import {readContract} from "@wagmi/core";
+import {useChainId,useConfig,useAccount} from "wagmi";
+import { chainsToTsSender,erc20Abi, tsenderAbi } from "@/constants";
 export default function AirdropForm() {
     const [tokenAddress, setTokenAddress] = useState<string>("");
     const [recipients, setRecipients] = useState(""); // State for recipients
     const [amounts, setAmounts] = useState("");     // State for amounts
+    
+    const account = useAccount();
+    const chainId = useChainId();
+    const config = useConfig();
+
+    async function getApprovedAmount(
+        // This function encapsulates the logic for calling allowance function on specified ERC20 token contract using `readContract` from `@wagmi/core`.
+        spenderAddress : `0x${string}`,
+        erc20TokenAddress : `0x${string}`,
+        ownerAddress : `0x${string}`
+    ): Promise<bigint> {
+        console.log("Checking allowance for token",erc20TokenAddress);
+        console.log("Spender",spenderAddress);
+        console.log("Owner",ownerAddress);
+        try {
+            const allowance = await readContract({
+                abi: erc20Abi,
+                address: erc20TokenAddress,
+                functionName: "allowance",
+                args: [ownerAddress, spenderAddress],
+            });
+            return allowance as bigint;
+        } catch (error) {
+            console.error("Error checking allowance", error);
+            throw new Error("Failed to check allowance");
+            // return BigInt(0);
+        }
+    }
+    
     async function handleSubmit() {
         // logic for submit the form
+        console.log("Form submitted");
+        console.log("Token address",tokenAddress);
+        console.log("Receipents",recipients);
+        console.log("Amounts",amounts);
+        console.log("ChainId",chainId);
+        const tsenderAddress = chainsToTsSender[chainId]?.tsender;
+        console.log("Deployed TSender address in this chain",tsenderAddress);
+        
+        /// Validation
+        if(!tsenderAbi) return;
+        if(!account.address) return;
+        if (!tokenAddress || !/^0x[a-fA-F0-9]{40}$/.test(tokenAddress)) return;
+        try {
+            const approvedAmount = await getApprovedAmount(
+                tsenderAddress as `0x${string}`,
+                tokenAddress as `0x${string}`,
+                account.address
+            );
+        } catch (error) {
+            console.error("Error in submission");
+        }
     }
+
     return (
         <div className="p-4 space-y-4"> {/* Add some padding/spacing */}
             <form onSubmit={(e) => {
